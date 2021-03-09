be70324f92723faa3a8293e617c8e8cc798ae6bf
Andre Sionek <andresionek91@gmail.com>
2021-03-09 23:54:34 +0000

    cria dms

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.common_stack import CommonStack
+from data_platform.dms.stack import DmsStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 common_stack = CommonStack(app)
+dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
