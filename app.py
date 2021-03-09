e71eef7e059aee80b2a8f8ac2d17c3e58e4f6410
Andre Sionek <andresionek91@gmail.com>
2021-03-09 23:47:56 +0000

    cria rds + vpc

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
+from data_platform.common_stack import CommonStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
+common_stack = CommonStack(app)
 app.synth()
