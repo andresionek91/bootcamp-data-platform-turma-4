647308122194168724f0cdfe0f39cff28192c409
Andre Sionek <andresionek91@gmail.com>
2021-03-11 22:54:04 +0000

    Deploy Redshift + Databricks

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
 from data_platform.kinesis.stack import KinesisStack
 from data_platform.glue_catalog.stack import GlueCatalogStack
+from data_platform.redshift.stack import RedshiftStack
+from data_platform.databricks.stack import DatabricksStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
@@ -13,4 +15,6 @@ dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.da
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 athena = AthenaStack(app)
+redshift = RedshiftStack(app, data_lake_raw=data_lake.data_lake_raw_bucket, data_lake_processed=data_lake.data_lake_processed_bucket, common_stack=common_stack)
+databricks = DatabricksStack(app)
 app.synth()
