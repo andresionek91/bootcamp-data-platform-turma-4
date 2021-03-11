c856623b4758a7cfdcb6050d78946ee63c3c3de0
Andre Sionek <andresionek91@gmail.com>
2021-03-11 21:44:55 +0000

    Adiciona Kinesis

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
+from data_platform.kinesis.stack import KinesisStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 common_stack = CommonStack(app)
 dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
