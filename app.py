e5222abc764348a39af9d474ee4b40f184178edd
Andre Sionek <andresionek91@gmail.com>
2021-03-11 22:08:28 +0000

    Cria glue

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from data_platform.data_lake.stack import DataLakeStack
 from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
 from data_platform.kinesis.stack import KinesisStack
+from data_platform.glue_catalog.stack import GlueCatalogStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 common_stack = CommonStack(app)
 dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 kinesis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 app.synth()
